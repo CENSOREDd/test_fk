6906641a50ee72faffc1c5d79c4b756278359199
MP Chromebook <michael.ji.park@gmail.com>
2019-09-06 16:42:50 -0700

    first commit

---
+++ fk.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from time import sleep
+
+print("what the fuck???")
+
+if __name__ == "__main__":
+    print("here is python code!!!")
+    print("Executing code...")
+    sleep(2)
